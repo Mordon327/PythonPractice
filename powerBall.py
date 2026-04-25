e6a8a5eccb95466797ebe3846e9bbb24fced3630
Shane Soderstrom <soderstromshane@gmail.com>
2026-04-25 13:03:21 -0600

    Cleaned up logic for numbers

--- powerBall.py
+++ powerBall.py
@@ -8,28 +8,13 @@ class PowerBall:
 		return f'{random.randrange(1, 100, 2)}'
 
 	def count(self):
-		if self.numOfBalls == 9:
+		if self.numOfBalls > 9:
 			return False
 		else:
 			return True
 
 ballCount = 0
 num1 = PowerBall()
-print(num1.returnNumbers())
-print(num1.count())
-print(num1.returnNumbers())
-print(num1.count())
-print(num1.returnNumbers())
-print(num1.count())
-print(num1.returnNumbers())
-print(num1.count())
-print(num1.returnNumbers())
-print(num1.count())
-print(num1.returnNumbers())
-print(num1.count())
-print(num1.returnNumbers())
-print(num1.count())
-print(num1.returnNumbers())
-print(num1.count())
-print(num1.returnNumbers())
-print(num1.count())
+while num1.count():
+	print(num1.returnNumbers())
+	print(num1.count())
