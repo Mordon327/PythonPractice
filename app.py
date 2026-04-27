86631b70139af8e1c9ec7ad4484bf27f5e93b613
Shane Soderstrom <soderstromshane@gmail.com>
2026-04-27 09:36:50 -0600

    Added background images and a few pages. Added style sheet.

--- app.py
+++ app.py
@@ -2,21 +2,19 @@ import os
 from flask import Flask, render_template, redirect, url_for, send_from_directory
 #When naming this file to app or wsgi, the command to run will be: flask run
 #Otherwise it will be flask --app filename run
-app = Flask(__name__)
 
+app = Flask(__name__)
 @app.route('/favicon.ico')
 def favicon():
-	return send_from_directory(os.path.join(app.root_path, 'static'),
-	'favicon.ico')
-
-@app.route("/hello")
-def hello_world():
-	return "<p>Hello, World!</p>"
-	#flask --app hello run
+	return send_from_directory(os.path.join(app.root_path, 'static'), 'img/favicon.ico')
 
 @app.route("/")
 def main_page():
 	return render_template('mainPage.html')
+
+@app.route("/contact")
+def contact():
+	return render_template('contactMePage.html')
 
 @app.route("/about")
 def about_page():
